e541c53ebafd39154d2d3667c94b42ad7d2c35d4
shanshan <87958246+lc-1010@users.noreply.github.com>
2022-09-01 16:46:28 +0800

    created remove cliam

--- test-7/substrate-node-template-polkadot-v0.9.25/pallets/poe/src/lib.rs
+++ test-7/substrate-node-template-polkadot-v0.9.25/pallets/poe/src/lib.rs
@@ -22,11 +22,11 @@ pub mod pallet{
 
     #[pallet::StorageMap]
     pub type Proofs<T: Config> = StorageMap<
-    _,
-    Blake2_128Concat,
-    BoundedVec<u8,T::MaxClinetLenght>,
-    (T::AccountId, T::BlockNumber),
-    >;
+                            _,
+                            Blake2_128Concat,
+                            BoundedVec<u8,T::MaxClinetLenght>,
+                            (T::AccountId, T::BlockNumber),
+                            >;
 
     pub enum Event<T:Confg>{
         ClaimCreated(T::AccountId,Vec<u8>),
@@ -43,5 +43,31 @@ pub mod pallet{
     
     #[pallet::hooks]
     impl<T:Config> Hooks<BlockNumberFor<T>> for Pallet<T>{}
+    
+    #[pallet::call]
+    impl<T:Config> Pallet<T>{
+        #[pallet::weight(0)]
+        pub fn create_claim(origin: OriginFor<T>,claim:Vec<u8>)-> DispatchResultWithPostInfo{
+            /// claim 存证 origin 发送方
+            let sender = ensure_signed(origin)?;
+            ensure!(!Proofs::<T>::contains_key(&claim),Error::<T>ProofAlreadyExist);
+            let currrent_block = <frame_support::Pallet<T>>::block_number();
+            Proofs::<T>::insert(&claim, (&sender,currrent_block));
+            Self::deposit_event(Event::ClaimCreated(sender,claim));
+            Ok(())
+        }
+
+        #[pallet::weight(0)]
+        pub fn remove_claim(origin:OriginFor<T> , claim:Vec<u8>)->DispatchResultWithPostInfo{
+            let sender =ensure_signed(origin)?;
+            ensure!(Proofs::<T>::contains_key(&claim),Error::<T>ClaimNotExist);
+            let (owner,_) = Proofs::<T>::get(&claim);
+            ensure!(sender == ownner,Error::<T>NotClaimOwner);
+            Proofs::<T>::remove(&claim);
+            Self::deposit_event(Event::ClaimRevoked(sender,claim));
+            Ok(())
+        }
+    }
+
     
 }
